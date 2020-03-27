9a08dce131e6984f6b9dab12a1b06a250a9c6a54
AssReact <huybinh020403@gmail.com>
2020-03-27 10:35:35 +0700

    ádsa

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var db = require('../server');
 router.use(bodyParser.json());
-
 /* GET home page. */
-
-
-
 router.get('/', function(req, res, next) {
 
     var sql = "SELECT * FROM user";
@@ -15,21 +11,61 @@ router.get('/', function(req, res, next) {
         if (err) {
             res.status(500).send({ error: 'Something failed!' });
         }
-        const mang = {"user":rowsn};
-        res.json(mang);
+
+        const mang = {"A":rowsn,"B":rowsn,"C":rowsn};
+        const mangm = {"mangtong" : mang};
+        res.json(mangm);
     })
 });
-router.get('/:id', function(req, res, next) {
-    const id = req.params.id;
-    const sql = `SELECT * FROM user WHERE id=${id}`;
-    db.query(sql, function(err, rowsn, fields) {
-        if (err) {
-            res.status(500).send({ error: 'Something failed!' });
-        }
-        const mang = {"user":rowsn};
-        res.json(mang);
-    })
-});
+// router.get('/:id', function(req, res, next) {
+//     const id = req.params.id;
+//     const sql = `SELECT * FROM user WHERE id=${id}`;
+//     db.query(sql, function(err, rowsn, fields) {
+//         if (err) {
+//             res.status(500).send({ error: 'Something failed!' });
+//         }
+//         const mang = {"user":rowsn};
+//         res.json(mang);
+//     })
+// });
+/*post method for create product*/ //đăng ký tài khoan
+// router.post('/create', function(req, res, next) {
+//     var phone = req.body.phone;
+//     var phone = req.body.phone;
+//     var sql = `INSERT INTO user (phone) VALUES ("${phone}")`;
+//     db.query(sql, function(err, result) {
+//         if(err) {
+//             res.status(500).send({ error: 'Something failed!' })
+//         }
+//         res.json({'status': 'success', id: result.insertId})
+//     })
+// });
+
+/*put method for update product*/
+// router.put('/update/:id', function(req, res, next) {
+//     var id = req.params.id;
+//     var phone = req.body.phone;
+//     var phone = req.body.phone;
+//     var sql = `UPDATE user SET phone="${phone}"  WHERE id=${id}`;
+//     db.query(sql, function(err, result) {
+//         if(err) {
+//             res.status(500).send({ error: 'Something failed!' })
+//         }
+//         res.json({'status': 'success'})
+//     })
+// });
+
+/*delete method for delete product*/
+// router.delete('/delete/:id', function(req, res, next) {
+//     var id = req.params.id;
+//     var sql = `DELETE FROM user WHERE id=${id}`;
+//     db.query(sql, function(err, result) {
+//         if(err) {
+//             res.status(500).send({ error: 'Something failed!' })
+//         }
+//         res.json({'status': 'success'})
+//     })
+// })
 
 module.exports = router;
 
